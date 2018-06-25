f834fca6762e4d00ae798f4690b953f0fd077d67
M <markakinshev@gmail.com>
2018-06-25 16:55:36 -0500

    Add files via upload

---
+++ downWithTheMatriarchy.js
@@ -0,0 +1,10 @@
+//down with chloe 
+
+downWithTheMatriarchy = function() {
+	
+	//client.channels.get('channelID').send("GAMERS! WE AWAKE \n@fae#2332 you will perish for your blasphemy\n:triumph: :triumph: :triumph: ");
+	client.on("message", function(message) {
+		message.channel.send('message')
+	});
+}
+downWithTheMatriarchy();
